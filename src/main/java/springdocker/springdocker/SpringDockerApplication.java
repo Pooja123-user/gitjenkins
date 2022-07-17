444aa9bdcfb99b1fac2ffb4f94f0926026756d0d
unknown <mindc1may392@iiht.tech>
2022-07-17 14:37:45 +0530

    add jenkins.demo

---
+++ src/main/java/springdocker/springdocker/SpringDockerApplication.java
@@ -0,0 +1,20 @@
+package springdocker.springdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringDockerApplication {
+	@GetMapping("/welcome")
+	public  String welcome() {
+		return "hello welocme to docker compose";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerApplication.class, args);
+	}
+
+}
